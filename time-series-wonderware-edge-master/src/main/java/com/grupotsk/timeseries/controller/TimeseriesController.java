430a64c7fd4228963ac2f37e9a00f465c59296fa
pgordotsk <119575101+pgordotsk@users.noreply.github.com>
2022-12-12 17:29:12 +0100

    Add files via upload

--- time-series-wonderware-edge-master/src/main/java/com/grupotsk/timeseries/controller/TimeseriesController.java
+++ time-series-wonderware-edge-master/src/main/java/com/grupotsk/timeseries/controller/TimeseriesController.java
@@ -4,19 +4,24 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.grupotsk.timeseries.model.PhysicalModel;
+import com.grupotsk.timeseries.model.Stops;
 import com.grupotsk.timeseries.model.Values;
 import com.grupotsk.timeseries.model.Atributes;
+import com.grupotsk.timeseries.model.NewStop;
 import com.grupotsk.timeseries.repository.AtributesRepository;
+import com.grupotsk.timeseries.repository.NewStopRespository;
 import com.grupotsk.timeseries.repository.PhysicalModelRepository;
+import com.grupotsk.timeseries.repository.StopsRepository;
 import com.grupotsk.timeseries.repository.ValuesRepository;
 
 @RestController
 
-@CrossOrigin(origins = "http://172.17.0.123")
+@CrossOrigin(origins = "http://localhost:4200")
 public class TimeseriesController {
 
 	@Autowired
@@ -25,6 +30,10 @@ public class TimeseriesController {
 	AtributesRepository atriRep;
 	@Autowired
 	ValuesRepository valRep;
+	@Autowired 
+	StopsRepository stopRep;
+	@Autowired 
+	NewStopRespository newStopRep;
 
 	//@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/physicalmodel")
@@ -45,8 +54,41 @@ public class TimeseriesController {
 			@RequestParam(value = "dateF", defaultValue = "") String dateF,
 			@RequestParam(value = "retrieval", defaultValue = "delta") String retrieval,
 			@RequestParam(value = "resolution", defaultValue = "100") Integer resolution){
-		
+
 		return valRep.find(name,dateI,dateF,retrieval,resolution); 
 	}
 
+	@GetMapping("/stopsMachine")
+	public List<Stops> stopList(@RequestParam(value = "name", defaultValue = "") String name){
+		return stopRep.find(name);
+	}
+
+	@CrossOrigin(origins = "http://localhost:4200")
+	@PostMapping("/insertStop")
+	public boolean registerStop(@RequestParam(value = "tagname", defaultValue = "") String tagname,
+			@RequestParam(value = "dateTime", defaultValue = "") String dateTime,
+			@RequestParam(value = "codParada", defaultValue = "") String codParada,
+			@RequestParam(value = "fechaProd", defaultValue = "") String fechaProd,
+			@RequestParam(value = "codMaqui", defaultValue = "") String codMaqui,
+			@RequestParam(value = "desMaqui", defaultValue = "") String desMaqui,
+			@RequestParam(value = "codTurno", defaultValue = "") String codTurno,
+			@RequestParam(value = "fechaIni", defaultValue = "") String fechaIni,
+			@RequestParam(value = "fechaFin", defaultValue = "") String fechaFin,
+			@RequestParam(value = "motivo", defaultValue = "") String motivo,
+			@RequestParam(value = "desMotivo", defaultValue = "") String desMotivo,
+			@RequestParam(value = "minutosPriParada", defaultValue = "") String minutosPriParada,
+			@RequestParam(value = "motivoSegParada", defaultValue = "") String motivoSegPar){
+
+
+		return newStopRep.insert2(tagname,dateTime,codParada,fechaProd,codMaqui,desMaqui,codTurno,fechaIni,fechaFin,motivo,desMotivo,minutosPriParada,motivoSegPar);
+	}
+	
+	@GetMapping("/viewStops")
+	public List<NewStop> stopsDesc(@RequestParam(value = "tagname", defaultValue = "") String name,
+			@RequestParam(value = "datetime", defaultValue = "") String date){
+		return newStopRep.viewStops(name,date);
+	}
+
+	
+	
 }
