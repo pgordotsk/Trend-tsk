b169c4e0fd7a92bfed3cabce3741ea25204d8888
pgordotsk <119575101+pgordotsk@users.noreply.github.com>
2022-12-15 16:52:18 +0100

    Add files via upload

--- time-series-wonderware-edge-master/src/main/java/com/grupotsk/timeseries/controller/TimeseriesController.java
+++ time-series-wonderware-edge-master/src/main/java/com/grupotsk/timeseries/controller/TimeseriesController.java
@@ -23,7 +23,8 @@ import com.grupotsk.timeseries.repository.ValuesRepository;
 
 @CrossOrigin(origins = "http://localhost:4200")
 public class TimeseriesController {
-
+	
+	// Conexion con los repositorios
 	@Autowired
 	PhysicalModelRepository phymodelRep;
 	@Autowired
@@ -35,18 +36,21 @@ public class TimeseriesController {
 	@Autowired 
 	NewStopRespository newStopRep;
 
+	// Obtener areas e instancias de la vista modelo
 	//@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/physicalmodel")
 	public List<PhysicalModel> areasCompleto(@RequestParam(value = "id", defaultValue = "829") Integer id){
 		return phymodelRep.findByAreaid(id);
 	}
 
+	// Obtener los atributos de una instancia
 	//@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/atributes")
 	public List<Atributes> atributesList(@RequestParam(value = "name", defaultValue = "") String name){
 		return atriRep.find(name);
 	}
 
+	// Obtener valores del un atributo y una fecha especificada
 	//@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/values")
 	public List<Values> vlues(@RequestParam(value = "name", defaultValue = "") String name,
@@ -58,14 +62,16 @@ public class TimeseriesController {
 		return valRep.find(name,dateI,dateF,retrieval,resolution); 
 	}
 
+	// Obtener paradas de la tabla CPMMAQUI y VSTPS_MOTIVINCID
 	@GetMapping("/stopsMachine")
 	public List<Stops> stopList(@RequestParam(value = "name", defaultValue = "") String name){
 		return stopRep.find(name);
 	}
 
+	// Registrar paradas en la tabla Stop
 	@CrossOrigin(origins = "http://localhost:4200")
 	@PostMapping("/insertStop")
-	public boolean registerStop(@RequestParam(value = "tagname", defaultValue = "") String tagname,
+	public void registerStop(@RequestParam(value = "tagname", defaultValue = "") String tagname,
 			@RequestParam(value = "dateTime", defaultValue = "") String dateTime,
 			@RequestParam(value = "codParada", defaultValue = "") String codParada,
 			@RequestParam(value = "fechaProd", defaultValue = "") String fechaProd,
@@ -77,12 +83,15 @@ public class TimeseriesController {
 			@RequestParam(value = "motivo", defaultValue = "") String motivo,
 			@RequestParam(value = "desMotivo", defaultValue = "") String desMotivo,
 			@RequestParam(value = "minutosPriParada", defaultValue = "") String minutosPriParada,
-			@RequestParam(value = "motivoSegParada", defaultValue = "") String motivoSegPar){
+			@RequestParam(value = "motivoSegParada", defaultValue = "") String motivoSegPar,
+			@RequestParam(value = "tiempoParada", defaultValue = "") String tiempoParada,
+			@RequestParam(value = "observaciones", defaultValue = "") String observaciones){
 
 
-		return newStopRep.insert2(tagname,dateTime,codParada,fechaProd,codMaqui,desMaqui,codTurno,fechaIni,fechaFin,motivo,desMotivo,minutosPriParada,motivoSegPar);
+		newStopRep.insert2(tagname,dateTime,codParada,fechaProd,codMaqui,desMaqui,codTurno,fechaIni,fechaFin,motivo,desMotivo,minutosPriParada,motivoSegPar,tiempoParada,observaciones);
 	}
 	
+	// Obtener paradas de la tabla Stops que hemos registrado
 	@GetMapping("/viewStops")
 	public List<NewStop> stopsDesc(@RequestParam(value = "tagname", defaultValue = "") String name,
 			@RequestParam(value = "datetime", defaultValue = "") String date){
